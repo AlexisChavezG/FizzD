6382deaba3df2db36b811cf920be6a0e84e2e696
Alexis Chavez <josechavezgaleas@compuAlexis.local>
2022-04-28 19:31:41 -0500

    Prueba de unidad

--- test/services/explorerServices.test.js
+++ test/services/explorerServices.test.js
@@ -3,7 +3,7 @@ const services = require('../../lib/services/explorerService')
 describe('Casos de pruebas',()=>{
     test('1. Primera funcion',()=>{
         const explorers = utils.readJsonFile('./explorers.json')
-        const lengt = services.filterByMission(explorers,"node")
+        const lengt = services.getAmountOfExplorersByMission(explorers,"node")
         expect(lengt).toBe(10)
     })
 })
