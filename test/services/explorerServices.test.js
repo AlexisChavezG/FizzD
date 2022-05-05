b1cd79b25a97757ad06e48f28803655aea32c71c
Alexis Chavez <josechavezgaleas@compuAlexis.local>
2022-05-04 20:36:32 -0500

    linter app

--- test/services/explorerServices.test.js
+++ test/services/explorerServices.test.js
@@ -1,9 +1,9 @@
-const utils = require('../../lib/utils/reader')
-const services = require('../../lib/services/explorerService')
-describe('Casos de pruebas',()=>{
-    test('1. Primera funcion',()=>{
-        const explorers = utils.readJsonFile('./explorers.json')
-        const lengt = services.getAmountOfExplorersByMission(explorers,"node")
-        expect(lengt).toBe(10)
-    })
-})
+const utils = require("../../lib/utils/reader");
+const services = require("../../lib/services/explorerService");
+describe("Casos de pruebas",()=>{
+    test("1. Primera funcion",()=>{
+        const explorers = utils.readJsonFile("./explorers.json");
+        const lengt = services.getAmountOfExplorersByMission(explorers,"node");
+        expect(lengt).toBe(10);
+    });
+});
