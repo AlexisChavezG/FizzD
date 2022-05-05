1b35d7630b46b223b17a3eb0df970218585e5a82
Alexis Chavez <josechavezgaleas@compuAlexis.local>
2022-05-04 20:26:43 -0500

    Se realiza modificaciones en la clase fizzBuzz

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const reader = require('./lib/utils/reader')
 const exploService = require('./lib/services/explorerService')
+const fizz = require('./lib/services/fizzbuzzService')
 const explorers = reader.readJsonFile('./explorers.json')
+
 const byMision = exploService.filterByMission(explorers,"node")
 console.log(byMision)
 const totalMision = exploService.getAmountOfExplorersByMission(explorers,"node")
 console.log(totalMision)
 const explorerByGit = exploService.getExplorersUsernamesByMission(explorers,"node")
 console.log(explorerByGit)
+const fizzBuz = fizz.applyValidationInExplorer(explorers)
+console.log(fizzBuz)
