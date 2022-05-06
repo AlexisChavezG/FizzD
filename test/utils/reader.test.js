671e9ef2c7fa42a01850e009fa5cf01c10e398ef
AlexisChavezG <alexis.chavez94@outlook.com>
2022-05-05 19:02:40 -0500

    Se define variable de arrya

--- test/utils/reader.test.js
+++ test/utils/reader.test.js
@@ -2,6 +2,6 @@ const utils = require("../../lib/utils/reader");
 describe("Casos de pruebas",()=>{
     test("1. Primera funcion",()=>{
         const explorers = utils.readJsonFile("./explorers.json");
-        expect(explorers).not.toBeNull(explorers);
+        expect(explorers).toBeDefined();
     });
 });
