c584375ac63115e752138eeec35ccc47a4fcde39
AlexisChavezG <alexis.chavez94@outlook.com>
2022-05-05 19:34:43 -0500

    Prueba de unidad para divisibles en 3 y 5

--- test/services/fizzbuzzServices.test.js
+++ test/services/fizzbuzzServices.test.js
@@ -6,18 +6,19 @@ describe("Casos de pruebas",()=>{
         const explorers = data.applyValidationInExplorer(explorer1); 
         expect(explorers.trick).toBe(1);
     });
-});
-describe("Casos de pruebas",()=>{
     test("Prueba para validación del mismo trick ",()=>{
         const explorer2 = {name: "Explorer2", score: 3}
         const explorers = data.applyValidationInExplorer(explorer2); 
         expect(explorers.trick).toBe("FIZZ");
     });
-});
-describe("Casos de pruebas",()=>{
     test("Prueba para validación del mismo trick ",()=>{
         const explorer3 = {name: "Explorer3", score: 5}
         const explorers = data.applyValidationInExplorer(explorer3); 
         expect(explorers.trick).toBe("BUZZ");
+    });
+    test("Prueba para validacion divisible en 3 y 5 ",()=>{
+        const explorer4 = {name: "Explorer4", score: 15}
+        const explorers = data.applyValidationInExplorer(explorer4); 
+        expect(explorers.trick).toBe("FIZZBUZZ");
     });
 });
