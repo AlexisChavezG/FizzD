c7534bec4c117d19052ad0ae2934dc185ea85be6
AlexisChavezG <alexis.chavez94@outlook.com>
2022-05-05 19:23:01 -0500

    Prueba automatica

--- test/services/fizzbuzzServices.test.js
+++ test/services/fizzbuzzServices.test.js
@@ -1,15 +1,15 @@
 const data = require("../../lib/services/fizzbuzzService");
 describe("Casos de pruebas",()=>{
     test("Prueba para validación del mismo trick ",()=>{
-        const explorer1 = {name: "Explorer1", score: 1}
-        const explorers = data.applyValidationInExplorer(explorer1) 
+        const explorer1 = {name: "Explorer1", score: 1};
+        const explorers = data.applyValidationInExplorer(explorer1); 
         expect(explorers.trick).toBe(1);
     });
 });
 describe("Casos de pruebas",()=>{
     test("Prueba para validación del mismo trick ",()=>{
-        const explorer1 = {name: "Explorer1", score: 1}
-        const explorers = data.applyValidationInExplorer(explorer1) 
+        const explorer1 = {name: "Explorer1", score: 1};
+        const explorers = data.applyValidationInExplorer(explorer1); 
         expect(explorers.trick).toBe(1);
     });
 });
