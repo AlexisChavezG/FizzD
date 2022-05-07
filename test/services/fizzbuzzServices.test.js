2eb2d9da3b1abe2e3957bb8be9142d546c5c82d9
AlexisChavezG <alexis.chavez94@outlook.com>
2022-05-07 16:00:13 -0500

    Se agregan coreccion by lint

--- test/services/fizzbuzzServices.test.js
+++ test/services/fizzbuzzServices.test.js
@@ -7,17 +7,17 @@ describe("Casos de pruebas",()=>{
         expect(explorers.trick).toBe(1);
     });
     test("Prueba para validación del mismo trick ",()=>{
-        const explorer2 = {name: "Explorer2", score: 3}
+        const explorer2 = {name: "Explorer2", score: 3};
         const explorers = data.applyValidationInExplorer(explorer2); 
         expect(explorers.trick).toBe("FIZZ");
     });
     test("Prueba para validación del mismo trick ",()=>{
-        const explorer3 = {name: "Explorer3", score: 5}
+        const explorer3 = {name: "Explorer3", score: 5};
         const explorers = data.applyValidationInExplorer(explorer3); 
         expect(explorers.trick).toBe("BUZZ");
     });
     test("Prueba para validacion divisible en 3 y 5 ",()=>{
-        const explorer4 = {name: "Explorer4", score: 15}
+        const explorer4 = {name: "Explorer4", score: 15};
         const explorers = data.applyValidationInExplorer(explorer4); 
         expect(explorers.trick).toBe("FIZZBUZZ");
     });
